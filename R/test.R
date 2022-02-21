509bf406a81a777a450bcde4f7ffc7f46882b354
Pawel Rucki <pawelrucki@gmail.com>
2022-02-21 13:55:44 +0100

    package files

---
+++ R/test.R
@@ -0,0 +1,28 @@
+#' Test function
+#'
+#' @export
+#' @examples
+#'
+#' test1()
+test1 <- function() {
+  print(1)
+}
+#' Test function
+#'
+#' @export
+#' @examples
+#'
+#' test2()
+test2 <- function() {
+  print(2)
+}
+
+#' Test function
+#'
+#' @export
+#' @examples
+#'
+#' test3()
+test3 <- function() {
+  print(3)
+}
