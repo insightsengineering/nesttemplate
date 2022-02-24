2db0c6ee2f446ef365e4bf8082f28d4947bd27e4
Pawel Rucki <pawelrucki@gmail.com>
2022-02-24 12:22:26 +0100

    Remove all dummy functions

--- R/test.R
+++ R/test.R
@@ -1,28 +1,5 @@
-#' Test function
-#'
-#' @export
-#' @examples
-#'
-#' test1()
-test1 <- function() {
-  print(1)
-}
-#' Test function
-#'
-#' @export
-#' @examples
-#'
-#' test2()
-test2 <- function() {
-  print(2)
-}
-
-#' Test function
-#'
-#' @export
-#' @examples
-#'
-#' test3()
-test3 <- function() {
-  print(3)
-}
+#' @description
+#' `nesttemplate` provides a custom [`pkgdown`](https://pkgdown.r-lib.org)
+#' template for NEST packages. Please don’t use it for your own package.
+#' @keywords internal
+"_PACKAGE"
