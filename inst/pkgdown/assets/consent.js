e71af57f4caff2849b6abd1d1934ed3b3f44a8a1
Pawel Rucki <pawelrucki@gmail.com>
2022-06-03 12:51:35 +0200

    modal to notification

--- inst/pkgdown/assets/consent.js
+++ inst/pkgdown/assets/consent.js
@@ -8,27 +8,6 @@
                 days: 15,
                 path: '/'
             },
-            website: {
-                name: '',
-                url: ''
-            },
-            logo: {
-                url: '',
-                width: '',
-                align: 'left',
-                margin: '0'
-            },
-            modal: {
-                hrColor: '#b3b3b3',
-                fontColor: '#3c3c3c',
-                backgroundColor: '#fafafa'
-            },
-            button: {
-                acceptColor: '#0a9919',
-                acceptColorHover: '#076212',
-                refuseColor: '#a40606',
-                refuseColorHover: '#7b0505',
-            },
             tag: {
                 cookiePrefix: '',
                 cookieDomain: '',
@@ -37,29 +16,6 @@
             },
             lang: 'en'
         }, options);
-        const content = {
-            title: {
-                en: `Data collection policy`
-            },
-            message: {
-                en: `<b>Cookie files are used to analyze ${params.website.url} website traffic by Google Analytics service.</b><br><br>Information about your browsing and use of the website is transmitted ${params.website.name.length > 0 ? ' to ' + params.website.name : ''} and will be analyzed <b>anonymously</b> to improve services. The data will be transmitted to the United States and are subject to <a href="https://policies.google.com/privacy?hl=en-US" target="_blank" title="Google privacy policy" style="color:inherit;font-weight:bold;">the Google privacy policy</a>.`
-            },
-            titleList: {
-                en: `List of cookies`
-            },
-            list: {
-                en: `<ul><li><b>_ga</b>: Used to distinguish users (expires after 2 years)</li><li><b>_gid</b>: Used to distinguish users (expires after 24 hours)</li><li><b>_gat</b>: Used to limit request rate (expires after 1 minute)</li></ul>`
-            },
-            conservation: {
-                en: `Your consent is kept for <b>${params.cookie.days}</b> day${params.cookie.days > 1 ? 's' : ''}. You can reset your consent by deleting the <b>${params.cookie.name}</b> cookie from your browser data.`
-            },
-            accepted: {
-                en: `Accept`
-            },
-            refused: {
-                en: `No thanks`
-            }
-        };
         const tag_params = {}
         for (const property in params.tag) {
             if (params.tag[property] !== '') {
@@ -74,56 +30,25 @@
             'gtag(\'config\', \'' + params.id + '\', ' + JSON.stringify(tag_params).replace(/"/g, '\'') + ');' +
             '</script>';
 
-        const logo = params.logo.url != '' ? `<div class="logo"><img src="${params.logo.url}" alt="Logo"></div>` : '';
-        const modal = '' +
-            '<div class="ck-modal">' +
-            '<div class="ck-window">' +
-            '<div class="ck-content">' +
-            '<span>' + logo + content.title[params.lang] + '</span>' +
-            '<hr>' +
-            '<p>' + content.message[params.lang] + '</p>' +
-            '<button class="accordion">' + content.titleList[params.lang] + '</button>' +
-            '<div class="panel">' + content.list[params.lang] + '</div>' +
-            '<p>' + content.conservation[params.lang] + '</p>' +
-            '</div> ' +
-            '<div class="ck-choise">' +
-            '<a class="btn-refuse" href="#">' + content.refused[params.lang] + '</a>' +
-            '<a class="btn-accept" href="#">' + content.accepted[params.lang] + '</a>' +
-            '</div> ' +
-            '</div> ' +
+        const cookieNotification = '' +
+            '<div class="cookie-consent">' +
+            '<span>This site uses cookies to enhance user experience. Please see <a href="cookie_policy.txt" class="ml-1 text-decoration-none">Privacy policy</a> </span>' +
+            '<div class="mt-2 d-flex align-items-center justify-content-center g-2">' +
+            '<button class="cookie-button" id="cookie_accept">Accept</button>' +
+            '<button class="cookie-button" id="cookie_deny">No thanks</button>' +
+            '</div>' +
             '</div>' +
             '<style>' +
-            '.ck-modal{display:flex;align-items:center;justify-content:center;position:fixed;width:100%;height:100%;top:0;left:0;background-color:rgba(0,0,0,.7);z-index:999;overflow-y:auto;}' +
-            '.ck-window{font-family:"Helvetica Neue", Helvetica, Arial, sans-serif;position:absolute;border-radius:5px;background-color:' + params.modal.backgroundColor + ';box-shadow:3px 3px 10px rgba(0,0,0,.5);}' +
-            '.ck-content{padding:15px;color:' + params.modal.fontColor + ';}' +
-            `.ck-content .logo{width:100%;text-align:${params.logo.align};}` +
-            `.ck-content .logo img{width:${params.logo.width};margin:${params.logo.margin};}` +
-            '.ck-content span{font-weight:300;}' +
-            '.ck-content b{font-weight:400;}' +
-            '.ck-content ul{margin:0;padding:0 20px 8px;}' +
-            '.ck-content p, .ck-content li{font-weight:200;margin:0.9em 0 1.0em!important;}' +
-            '.ck-content hr{height:1px;margin:15px 0;border:0;border-top:1px solid ' + params.modal.hrColor + ';}' +
-            '.ck-content .accordion{background-color:#e2e2e2;color:#444;font-size:1em;font-weight:600;cursor:pointer;padding:10px;width:100%;text-align:left;border:none;outline:none;transition:background-color 0.4s ease-in-out;}' +
-            '.ck-content .accordion:after{content:\'\\002B\';color:#777;font-weight:bold;float:right;margin-left:5px;}' +
-            '.ck-content .accordion.active:after{content:\'\\002D\'}' +
-            '.ck-content .accordion.active, .ck-content .accordion:hover{background-color:#d7d7d7;}' +
-            '.ck-content .panel{height:0;padding:0 5px;background-color:transparent;border:1px solid #d7d7d7;visibility:hidden;}' +
-            '.ck-choise{display:flex;align-items:stretch;justify-content:center;}' +
-            '.ck-choise .btn-refuse, .ck-choise .btn-accept{display:block;width:50%;font-size:0.9em;font-weight:bold;text-transform:uppercase;text-decoration:none;padding:10px 5px;text-align:center;transition:background-color .5s ease;}' +
-            '.ck-choise .btn-refuse{color:#fff;background-color:' + params.button.refuseColor + ';border-radius:0 0 0 3px;}' +
-            '.ck-choise .btn-refuse:hover, .ck-choise .btn-refuse:focus{color:#fff;background-color:' + params.button.refuseColorHover + ';text-decoration:none;}' +
-            '.ck-choise .btn-accept{color:#fff;background-color:' + params.button.acceptColor + ';border-radius:0 0 3px 0;}' +
-            '.ck-choise .btn-accept:hover, .ck-choise .btn-accept:focus{color:#fff;background-color:' + params.button.acceptColorHover + ';text-decoration:none;}' +
-            '@media (max-width: 576px) { .ck-modal .ck-window{width:90%;}.ck-content p, .ck-content li{font-size:0.9em;}.ck-content span{font-size:1.3em;}.ck-content .logo{display:none;} }' +
-            '@media (min-width: 576px) { .ck-modal .ck-window{width:70%;}.ck-content p, .ck-content li{font-size:1.0em;}.ck-content span{font-size:1.4em;}.ck-content .logo{display:block;} }' +
-            '@media (min-width: 769px) { .ck-modal .ck-window{width:50%;}.ck-content p, .ck-content li{font-size:1.1em;}.ck-content span{font-size:1.5em;} }' +
-            '</style>';
+            '.cookie-consent{ position: fixed; bottom: 8px; left: 20px; width: 260px; color: #fff; line-height: 20px; padding-block: 7px; padding-left: 10px; padding-right: 10px; font-size: 14px; background: #292929; z-index: 120; cursor: pointer; border-radius: 3px; }' +
+            '.cookie-button{ height: 20px; width: 104px; color: #fff; font-size: 12px; line-height: 10px; border-radius: 3px; border: 1px solid green; background-color: green; margin-inline: auto; }' +
+            '</style>' +
+            '';
         let h;
         function init() {
             if (params.id != '') {
                 let c = getCookie();
                 if (c == null || (c != 0 && c != 1)) {
-                    displayModal();
+                    displayCookieNotification();
                 } else if (c == 1) {
                     addTag();
                 }
@@ -131,47 +56,22 @@
                 console.log('No ID defined in the cookieWall params.');
             }
         }
-        function displayModal() {
-            $('body').prepend(modal);
-            h = $('.ck-modal .panel ul').outerHeight(true);
-            $('body').on('mousedown', '.ck-modal .ck-choise a', setChoise);
-            $('body').on('click', '.accordion', function () {
-                this.classList.toggle('active');
-                let panel = $(this).parent().find('.panel');
-                if (panel.css('visibility') === 'visible') {
-                    $(panel).animate({
-                        height: 0
-                    }, 200, function () {
-                        panel.css('visibility', 'hidden');
-                    });
-                } else {
-                    panel.css('visibility', 'visible');
-                    $(panel).animate({
-                        height: h
-                    }, 200);
-                }
-            });
-            $(window).on('resize', function (e) {
-                h = $('.ck-modal .panel ul').outerHeight(true);
-                let panel = $('.ck-modal .panel');
-                if (panel.css('visibility') === 'visible') {
-                    panel.css('height', h);
-                }
-            });
+        function displayCookieNotification() {
+            $('body').prepend(cookieNotification);
+            $('body').on('mousedown', '.cookie-consent .cookie-button', setChoise);
+        }
+        function removeCookieNotification() {
+            $('body .cookie-consent').remove();
         }
         function setChoise(e) {
             e.preventDefault();
-            let r = $(this).attr('class').substring(4, $(this).attr('class').length);
-            if (r == 'accept') {
+            if ($(this).id == 'cookie_accept') {
                 setCookie(1);
                 addTag();
             } else {
                 setCookie(0);
             }
-            $('body .ck-modal').remove();
-            $('body').off('mousedown', '.ck-modal .ck-choise a');
-            $('body').off('click', '.accordion');
-            $(window).off('resize');
+            removeCookieNotification();
         }
         function addTag() {
             $('body').append(tag);
